1695abebb3971561bf2ed21e6c10615f60bc3d4a
fRomanSt <vodaz@mail.ru>
2024-12-13 13:00:32 +0300

    first

---
+++ BR.py
@@ -0,0 +1,46 @@
+import flet as ft
+
+
+class BoevRasch(ft.UserControl):
+
+    def build(self):
+
+        self.t = ft.Text(value='Укажи какая смена заступает')
+        self.b = ft.ElevatedButton(text='Начать формирование БР', on_click=self.add_clicked)
+        self.cg = ft.RadioGroup(content=ft.Row([
+            ft.Radio(value="1",label="1 смена"),
+            ft.Radio(value="2", label="2 смена"),
+            ft.Radio(value="3",label="3 смена"),
+            ft.Radio(value="4",label="4 смена"),
+            ft.Radio(value="5", label="5 смена")]))
+
+        return ft.Column(
+            width=600,
+            controls=[
+                self.t,
+                ft.Row(
+                    controls=[
+                        self.cg,
+                    ],
+                ),
+                self.b
+            ],
+        )
+
+    def add_clicked(self, e):
+
+        self.t.value = f"Your favorite color is: {self.cg.value}"
+        self.update()
+
+
+
+def main(page: ft.Page):
+    page.title = "Бомбовый расчет"
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.update()
+
+    boevRasch = BoevRasch()
+
+    page.add(boevRasch)
+
+ft.app(target=main)
---
+++ Proba.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+from scipy.stats import chi2_contingency, chi2
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pingouin as pg
+
+
+print(__import__('BR').__class__.cg)
+
+
+
+
+
+
+#_____________________________________________________________ получение файлов
+# home_directory = 'C:/Users/fRomanSt/Desktop/frasch/'
+#
+# active_studs = pd.read_excel(home_directory+'1.xlsx')
+# print(active_studs)
